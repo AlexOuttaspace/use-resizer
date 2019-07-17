b3d874de3523614e5cdefe899a69868241951a91
AlexOuttaspace <matanforeveryone@gmail.com>
2019-07-17 19:53:45 +0300

    handle changing size

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import { useResizer } from './resizer'
+import { useResizer, ResizeDirection } from './resizer'
 
-const blockStyle = {
-  width: 100,
-  height: 100,
-  border: '1px solid green'
+const wrapperStyle: React.CSSProperties = {
+  position: 'absolute',
+  left: 30,
+  top: 30
+}
+
+const getHandleStyle = (direction: ResizeDirection): React.CSSProperties => {
+  const position = (() => {
+    let left: string | number = '50%'
+    if (direction.toLowerCase().includes('left')) left = 0
+    if (direction.toLowerCase().includes('right')) left = '100%'
+
+    let top: string | number = '50%'
+    if (direction.toLowerCase().includes('top')) top = 0
+    if (direction.toLowerCase().includes('bottom')) top = '100%'
+    return { left, top }
+  })()
+
+  return {
+    ...position,
+    transform: "translate(-50%, -50%)",
+    position: 'absolute',
+    cursor: 'grab',
+    width: 10,
+    height: 10,
+    border: '1px solid blue',
+    fontSize: 5
+  }
 }
 
 const App: React.FC = () => {
-  const { top, left, right, bottom, topRight, topLeft, bottomLeft, bottomRight } = useResizer()
+  const [size, setSize] = useState({width: 100, height: 100})
+
+  const { top, left, right, bottom, topRight, topLeft, bottomLeft, bottomRight } = useResizer({
+    onResize: ({ width, height}) => setSize({ width, height }),
+    size
+  })
 
   return (
     <React.Fragment>
-      <div {...top} style={blockStyle}>top</div>
-      <div {...left} style={blockStyle}>left</div>
-      <div {...right} style={blockStyle}>right</div>
-      <div {...bottom} style={blockStyle}>bottom</div>
-      <div {...topRight} style={blockStyle}>topRight</div>    
-      <div {...topLeft} style={blockStyle}>topLeft</div>    
-      <div {...bottomLeft} style={blockStyle}>bottomLeft</div>    
-      <div {...bottomRight} style={blockStyle}>bottomRight</div>        
+      <div style={{...wrapperStyle, ...size}}>
+        <div {...top} style={getHandleStyle('top')}>top</div>
+        <div {...left} style={getHandleStyle('left')}>left</div>
+        <div {...right} style={getHandleStyle('right')}>right</div>
+        <div {...bottom} style={getHandleStyle('bottom')}>bottom</div>
+        <div {...topRight} style={getHandleStyle('topRight')}>topRight</div>    
+        <div {...topLeft} style={getHandleStyle('topLeft')}>topLeft</div>    
+        <div {...bottomLeft} style={getHandleStyle('bottomLeft')}>bottomLeft</div>    
+        <div {...bottomRight} style={getHandleStyle('bottomRight')}>bottomRight</div>        
+      </div>
     </React.Fragment>
   );
 }
--- src/resizer.ts
+++ src/resizer.ts
@@ -1,10 +1,9 @@
-import { useState, useCallback, useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 
-type ResizeDirection = 'top' | 'topRight' | 'right' | 'bottomRight' | 'bottom' | 'bottomLeft' | 'left' | 'topLeft'
+export type ResizeDirection = 'top' | 'topRight' | 'right' | 'bottomRight' | 'bottom' | 'bottomLeft' | 'left' | 'topLeft'
 type TPointCoords = { x: number, y: number }
 type TCreatePointerDownHandler = (resizeDirection: ResizeDirection) => TOnPointerDown
 type TOnPointerDown = (event: React.PointerEvent<HTMLElement>) => void
-const allowedResizeDirections: ResizeDirection[] = ['top' , 'topRight' , 'right' , 'bottomRight' , 'bottom' , 'bottomLeft' , 'left' , 'topLeft']
 
 type HandleProps = {
   onPointerDown: TOnPointerDown
@@ -14,29 +13,61 @@ type HandlePropsMap = {
   [key in ResizeDirection]: HandleProps
 }
 
+export interface TElementSize {
+  width: number
+  height: number
+}
+
+interface TUseResizerOptions {
+  onResize?: (size: { width: number, height: number }) => void
+  size: TElementSize
+}
+
+const blockTextSelection = (e: React.PointerEvent<HTMLElement> | PointerEvent): void =>{
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+const allowedResizeDirections: ResizeDirection[] = ['top' , 'topRight' , 'right' , 'bottomRight' , 'bottom' , 'bottomLeft' , 'left' , 'topLeft']
+
 const createHandlePropsForDirection = (direction: ResizeDirection, createPointerDown: TCreatePointerDownHandler) => {
   return {
     onPointerDown: createPointerDown(direction)
   }
 }
 
-const createMoveHandler = (initialCoords: TPointCoords, resizeDirection: ResizeDirection): any => {
-  console.log(initialCoords)
-  return (e: any) => console.log(initialCoords, resizeDirection)
+const getEventCoordinates = ({ clientX, clientY }: PointerEvent): TPointCoords => ({
+  x: clientX,
+  y: clientY
+})
+
+const createMoveHandler = (initialCoords: TPointCoords, resizeDirection: ResizeDirection, resizerOptions: TUseResizerOptions): any => {
+  const { size: { width, height }, onResize } = resizerOptions
+
+  return (event: PointerEvent) => {
+   
+    const currentPointerCoords = getEventCoordinates(event)
+
+    const displaysments: TPointCoords = {
+      x: currentPointerCoords.x - initialCoords.x,
+      y: currentPointerCoords.y - initialCoords.y
+    }
+
+    onResize && onResize({ width: width + displaysments.x, height: height + displaysments.y })
+  }
 }
 
-export const useResizer = (): HandlePropsMap => {
+export const useResizer = (options: TUseResizerOptions): HandlePropsMap => {
 
   const createPointerDownHandler = useCallback<TCreatePointerDownHandler>((resizeDirection) => (event) => {
-    /*
-      Save initial coords of the interaction
-    */
-    const initialCoords: TPointCoords = { x: event.clientX, y: event.clientY }
+    blockTextSelection(event)
+
+    const initialCoords: TPointCoords = getEventCoordinates(event.nativeEvent)
 
     /*
-      Use enclosure to preserve inital coords of interaction
+      Use closure that preserves inital coords of interaction
     */
-    const moveHandler = createMoveHandler(initialCoords, resizeDirection)
+    const moveHandler = createMoveHandler(initialCoords, resizeDirection, options)
 
     /*
       Add event listener to handle pointer motion
@@ -57,14 +88,14 @@ export const useResizer = (): HandlePropsMap => {
       */
       once: true
     })
-  }, [])
+  }, [options])
 
-  const handlePropsMap: HandlePropsMap = allowedResizeDirections.reduce((acc, direction) => {
+  const handlePropsMap = useMemo<HandlePropsMap>(() => allowedResizeDirections.reduce((acc, direction) => {
     return {
       ...acc,
       [direction]: createHandlePropsForDirection(direction, createPointerDownHandler)
     }
-  }, {} as any)
+  }, {} as any), [createPointerDownHandler])
 
   return handlePropsMap
 }
